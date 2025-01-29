43d2849f7fae7cfda7daf5f176da8a269b376635
Soulo8 <soulo08@outlook.com>
2025-01-29 11:28:41 +0100

    Tags create and edit used custom events with outputs

--- src/app/tags/tag-form/tag-form.component.ts
+++ src/app/tags/tag-form/tag-form.component.ts
@@ -1,12 +1,8 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit, inject, output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
-import { Tag } from '../shared/tag.model';
-import { TagService } from '../shared/tag.service';
 
 @Component({
   selector: 'app-tag-form',
@@ -15,53 +11,19 @@ import { TagService } from '../shared/tag.service';
   styleUrl: './tag-form.component.css'
 })
 export class TagFormComponent implements OnInit {
-  tag$!: Observable<Tag>;
-  tagId = '';
-
-  constructor(private tagService: TagService) {}
-
-  @Input()
-  set id(tagId: string) {
-    if (tagId !== undefined) {
-      this.tagId = tagId;
-      this.tag$ = this.tagService.getTag(tagId);
-    }
-  }
+  initialized = output<FormGroup>();
+  formSubmitted = output<FormGroup>();
 
   private formBuilder = inject(FormBuilder);
   tagForm = this.formBuilder.group({
     name: ['', Validators.required]
   });
 
-  private readonly router = inject(Router);
-
   ngOnInit(): void {
-    if (this.tag$ !== undefined) {
-      this.tag$.subscribe(tag => {
-        this.tagForm.patchValue(tag);
-      });
-    }
+    this.initialized.emit(this.tagForm);
   }
 
   onSubmit() {
-    const tag: Tag = {
-      name: this.tagForm.value.name ?? ''
-    };
-
-    if (this.tag$ !== undefined) {
-      tag.id = parseInt(this.tagId);
-
-      this.tagService.updateTag(tag).subscribe(() => {
-        this.goToTagList();
-      });
-    } else {
-      this.tagService.addTag(tag).subscribe(() => {
-        this.goToTagList();
-      });
-    }
-  }
-
-  goToTagList() {
-    this.router.navigate(['tags']);
+    this.formSubmitted.emit(this.tagForm);
   }
 }
